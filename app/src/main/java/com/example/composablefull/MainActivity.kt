5f1b64f7749d8226affaf04ad0c90c4c9d307aca
Lokesh Patel <lokesh.patel@jet2traveltech.com>
2025-03-31 22:48:53 +0530

    SideEffect composable

--- app/src/main/java/com/example/composablefull/MainActivity.kt
+++ app/src/main/java/com/example/composablefull/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.composablefull.projects.NoteCounterApp
 import com.example.composablefull.ui.theme.ComposableFullTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +21,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposableFullTheme {
-                Basic()
+//                Basic()
+//                Buttons()
+//                StateHoisting()
+//                NoteCounterApp()
+                SideEffect()
             }
         }
     }
